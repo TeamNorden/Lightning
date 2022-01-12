0a9a1e8ce56c9995bc05a5e75599db736cb78fe7
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-12 22:34:04 +0500

    added command handler (check desc)
    All handler files will use the `*.handler.ts` naming convention

---
+++ src/modules/handlers/command.handler.ts
@@ -0,0 +1,39 @@
+import fs, { readdirSync } from 'fs'
+import path from 'path'
+import { LTNClient } from '../../client'
+import ICommand from '../../typings/ICommand'
+import { Class } from 'type-fest'
+import BaseCommand from '../../bases/command/BaseCommand'
+
+const commandHandler = async (client: LTNClient) => {
+    const { categories } = client
+    const { structureType } = client.config
+
+    for (let folder of categories) {
+        let folderFiles = readdirSync(folder).filter(file => ['js', 'ts'].includes(path.extname(file)))
+
+        for (let file of folderFiles) {
+            let req = await import(file)
+
+            let command: ICommand | BaseCommand
+
+            if (structureType == 'OBJECT') {
+                command = req.default as ICommand
+
+                client.commands.set(command.data.name, command)
+            } else {
+                let CommandClass = req.default as Class<BaseCommand>
+
+                command = new CommandClass()
+
+                if (!(command instanceof BaseCommand)) {
+                    throw new TypeError(`Command ${command.data.name}doesn't seem to be an instance of BaseCommand. Did you forget extending it?`)
+                }
+            }
+
+            client.commands.set(command.data.name, command)
+        }
+    }
+}
+
+export default commandHandler
