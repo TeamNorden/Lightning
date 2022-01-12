f50ff24e3ff4847489c716e0b6ff69e10499bfca
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-12 23:31:05 +0500

    The amount of pain i experienced trying to set this up cannot be described using simple numbers

---
+++ __tests__/index.test.ts
@@ -0,0 +1,7 @@
+import assert from 'assert'
+
+describe('Client Tests', () => {
+    it('should equal to 4', function() {
+        assert.equal(2 + 2, 4, 'This is not supposed to happen')
+    })
+})
