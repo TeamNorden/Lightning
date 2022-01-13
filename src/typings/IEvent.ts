6194beedcdd6ea53f191c96c8a48fdf8ac06043d
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-14 00:13:40 +0500

    Added event interfaces

---
+++ src/typings/IEvent.ts
@@ -0,0 +1,18 @@
+import { LTNClient } from '../client'
+
+export interface IEventData {
+    name: string,
+    once?: boolean
+}
+
+export interface IEventExec {
+    (
+        client: LTNClient,
+        ...args: string[]
+    ): any
+}
+
+export default interface IEvent {
+    data: IEventData,
+    exec: IEventExec
+}
