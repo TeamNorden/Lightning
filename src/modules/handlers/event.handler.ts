743a3ba995dbbf9030e1e0c87b4be09ec7450b6e
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-14 00:13:29 +0500

    Event handler finally here big pog

---
+++ src/modules/handlers/event.handler.ts
@@ -0,0 +1,31 @@
+import { LTNClient } from '../../client'
+import { readdirSync } from 'fs'
+import IEvent from '../../typings/IEvent'
+import BaseEvent from '../../bases/event/BaseEvent'
+import { Class } from 'type-fest'
+
+const eventHandler = async (client: LTNClient) => {
+    const { structureType } = client.config
+
+    let eventFiles = readdirSync(client.eventDir)
+
+    for (let file of eventFiles) {
+        let req = await import(file)
+
+        let event: IEvent | BaseEvent
+
+        if (structureType === 'OBJECT') {
+            event = req.default as IEvent
+        } else {
+            let EventClass = req.default as Class<BaseEvent>
+
+            event = new EventClass()
+
+            if (!(event instanceof BaseEvent)) {
+                throw new TypeError(`Command ${event.data.name}doesn't seem to be an instance of BaseEvent. Did you forget extending it?`)
+            }
+        }
+
+        client.events.set(event.data.name, event)
+    }
+}
