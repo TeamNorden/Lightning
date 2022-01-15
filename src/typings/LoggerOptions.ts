8d3435de3a84ae4658160b03320d6786f1999324
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-16 01:27:02 +0500

    Added Logger Types

---
+++ src/typings/LoggerOptions.ts
@@ -0,0 +1,14 @@
+export type RGBColor = [number, number, number]
+
+export type LTNLoggerType = 'LOGIN' | 'COMMAND' | 'EVENT' | 'READY' | 'LOAD' | 'DATABASE'
+
+export type LTNLoggerUtilsType = 'ERROR' | 'WARN'
+
+export type LTNLoggerOptions = {
+    primary: RGBColor
+    secondary: RGBColor
+} & {
+    [key in LTNLoggerType]?: RGBColor
+} & {
+    [key in LTNLoggerUtilsType]?: RGBColor
+}
