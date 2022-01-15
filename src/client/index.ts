b8696dc8aa3e83a8f2b494d8c5e2d7ae6e670f08
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-16 01:27:36 +0500

    Added LTNLogger as a client property

--- src/client/index.ts
+++ src/client/index.ts
@@ -13,6 +13,9 @@ import { IClientOptions, IClientConfig, ICommand, IEvent } from '../typings'
 import eventHandler from '../modules/handlers/event.handler'
 import commandHandler from '../modules/handlers/command.handler'
 
+// Utils
+import LTNLogger from '../modules/utils/Logger'
+
 export class LTNClient extends Client {
     // Commands And Events
     public commands = new Map<string, ICommand | BaseCommand>()
@@ -28,6 +31,9 @@ export class LTNClient extends Client {
     // User Configuration
     public config: IClientConfig
 
+    // Logger
+    public logger: LTNLogger
+
     // Handlers
     private _loadCommands = commandHandler
     private _loadEvents = eventHandler
@@ -41,12 +47,18 @@ export class LTNClient extends Client {
         this.categories = readdirSync(this.commandDir)
 
         this.config = options.config
+
+        this.logger = new (options.logger?.class ?? LTNLogger)(options.logger?.options ?? {
+            primary: [209, 35, 49],
+            secondary: [255, 87, 87]
+        })
     }
 
-    public async start(token: string) {
+    public async start(token: string, silent: boolean = true) {
         await this._loadCommands(this)
         await this._loadEvents(this)
 
-        this.loginBot(token)
+        await this.loginBot(token)
+        if (!silent) this.logger.log('LOGIN', 'Bot has logged in! Thanks for using LTN!')
     }
 }
