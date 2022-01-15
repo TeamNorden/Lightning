38e4ff1b148bda94fec8118434cc4eecd13b24be
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-16 01:38:02 +0500

    this file is now a client property now

--- src/modules/utils/matchTypes.ts
+++
@@ -1,9 +0,0 @@
-const matchTypes = (element: any, type: string) => {
-    if (type === 'ARRAY' && Array.isArray(type)) return
-
-    if (typeof element === type.toLowerCase()) return
-
-    throw new TypeError(
-        `'${element}' is supposed to be of type '${type.toUpperCase()}'`
-    )
-}
