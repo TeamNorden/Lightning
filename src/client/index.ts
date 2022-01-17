ada23c4fe7503dc8e07ab39c058286b2e5f5ad63
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-17 22:07:51 +0500

    added prefix property, messageHandler, and formatted code

--- src/client/index.ts
+++ src/client/index.ts
@@ -7,11 +7,18 @@ import BaseCommand from '../bases/command/BaseCommand'
 import BaseEvent from '../bases/event/BaseEvent'
 
 // Typings
-import { IClientConfig, IClientOptions, ICommand, IEvent, Strictness } from '../typings'
+import {
+    IClientConfig,
+    IClientOptions,
+    ICommand,
+    IEvent,
+    Strictness
+} from '../typings'
 
 // Handlers
 import eventHandler from '../modules/handlers/event.handler'
 import commandHandler from '../modules/handlers/command.handler'
+import messageHandler from '../modules/handlers/message.handler'
 
 // Utils
 import LTNLogger from '../modules/utils/Logger'
@@ -20,6 +27,9 @@ export class LTNClient extends Client {
     // Commands And Events
     public commands = new Map<string, ICommand | BaseCommand>()
     public events = new Map<string, IEvent | BaseEvent>()
+
+    // Prefix (default: '!')
+    public prefix = '!'
 
     // Directories
     public readonly commandDir: string
@@ -37,6 +47,13 @@ export class LTNClient extends Client {
     // Handlers
     private _loadCommands = commandHandler
     private _loadEvents = eventHandler
+    private _loadMessageHandler = (
+        handler: typeof messageHandler = messageHandler
+    ) => {
+        this.on('message', (message) => {
+            handler(this, message)
+        })
+    }
 
     constructor(options: IClientOptions, revoltOptions: RevoltOptions) {
         super(revoltOptions)
@@ -48,10 +65,19 @@ export class LTNClient extends Client {
 
         this.config = options.config
 
-        this.logger = new (options.logger?.class ?? LTNLogger)(options.logger?.options ?? {
-            primary: [209, 35, 49],
-            secondary: [255, 87, 87]
-        }, this.config.strictness ?? Strictness.MODERATE)
+        if (
+            !this.config.messageHandler ||
+            this.config.messageHandler === 'DEFAULT'
+        )
+            this.config.messageHandler = messageHandler
+
+        this.logger = new (options.logger?.class ?? LTNLogger)(
+            options.logger?.options ?? {
+                primary: [209, 35, 49],
+                secondary: [255, 87, 87]
+            },
+            this.config.strictness ?? Strictness.MODERATE
+        )
     }
 
     public matchTypes = (type: string, element: any) => {
@@ -64,11 +90,18 @@ export class LTNClient extends Client {
         )
     }
 
-    public async start(token: string, silent: boolean = true) {
+    public start = async (token: string, silent: boolean = true) => {
         await this._loadCommands(this)
         await this._loadEvents(this)
 
+        if (this.config.messageHandler !== 'NONE')
+            await this._loadMessageHandler(
+                this.config.messageHandler as typeof messageHandler
+            )
+
         await this.loginBot(token)
-        if (!silent) this.logger.log('LOGIN', 'Bot has logged in! Thanks for using LTN!')
+
+        if (!silent)
+            this.logger.log('LOGIN', 'Bot has logged in! Thanks for using LTN!')
     }
 }
