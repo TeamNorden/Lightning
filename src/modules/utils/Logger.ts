ac8dbd48b9a1ab4bad4482cf8ed4209565b56162
Fad F <90547391+FadTheChad@users.noreply.github.com>
2022-01-17 22:08:12 +0500

    prettier logger code

--- src/modules/utils/Logger.ts
+++ src/modules/utils/Logger.ts
@@ -17,37 +17,66 @@ class LTNLogger {
             EVENT: options.EVENT ?? [5, 247, 82],
             ERROR: options.ERROR ?? [0, 0, 255],
             WARN: options.WARN ?? [255, 174, 0],
-            DATABASE: options.DATABASE ?? [77, 179, 61],
+            DATABASE: options.DATABASE ?? [77, 179, 61]
         }
 
         this.strictness = strictness
     }
 
-    public log = (type: LTNLoggerType, message: any, tab?: boolean, newLineBefore?: boolean) => {
-        let colourOfType = this.colours[type.toLowerCase() as keyof LTNLoggerOptions]!
+    public log = (
+        type: LTNLoggerType,
+        message: any,
+        tab?: boolean,
+        newLineBefore?: boolean
+    ) => {
+        let colourOfType =
+            this.colours[type.toLowerCase() as keyof LTNLoggerOptions]!
 
         let chalkColour = chalk.rgb(...colourOfType)
 
-        let typeTitle = chalkColour(type.trim().split(/(?=[A-Z])/).join('_').toUpperCase())
+        let typeTitle = chalkColour(
+            type
+                .trim()
+                .split(/(?=[A-Z])/)
+                .join('_')
+                .toUpperCase()
+        )
 
         if (['READY', 'LOGIN'].includes(type)) {
-            chalkColour = chalk.rgb(...this.colours.secondary).bgRgb(...colourOfType)
+            chalkColour = chalk
+                .rgb(...this.colours.secondary)
+                .bgRgb(...colourOfType)
         }
 
-        let data = (newLineBefore ? '\n' : '') + (tab ? '\t' : '') + '[' + typeTitle + '] ' + chalkColour(message)
+        let data =
+            (newLineBefore ? '\n' : '') +
+            (tab ? '\t' : '') +
+            '[' +
+            typeTitle +
+            '] ' +
+            chalkColour(message)
 
         console.log(data)
     }
 
-    public error = (err: any, errType?: LTNLoggerType, crashOnErr?: boolean) => {
+    public error = (
+        err: any,
+        errType?: LTNLoggerType,
+        crashOnErr?: boolean
+    ) => {
         let errColourArray = this.colours.ERROR!
         let errColour = chalk.rgb(...errColourArray)
 
-        let data = '[' + errColour(errType ? (errType.toUpperCase() + '_ERR') : 'ERR') + '] ' + errColour(err)
+        let data =
+            '[' +
+            errColour(errType ? errType.toUpperCase() + '_ERR' : 'ERR') +
+            '] ' +
+            errColour(err)
 
         console.log(data)
 
-        if (crashOnErr === undefined) crashOnErr = this.strictness === Strictness.HIGH
+        if (crashOnErr === undefined)
+            crashOnErr = this.strictness === Strictness.HIGH
 
         // If the user made the config Strict, crash on recieving an err unless specifically told not to
         if (crashOnErr) process.exit(1)
@@ -57,7 +86,11 @@ class LTNLogger {
         let warnColourArray = this.colours.WARN!
         let warnColour = chalk.rgb(...warnColourArray)
 
-        let data = '[' + warnColour(warnType ? (warnType.toUpperCase() + '_WARN') : 'WARN') + '] ' + warnColour(err)
+        let data =
+            '[' +
+            warnColour(warnType ? warnType.toUpperCase() + '_WARN' : 'WARN') +
+            '] ' +
+            warnColour(err)
 
         console.log(data)
     }
