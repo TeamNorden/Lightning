27a81d8149dbc5bca1b0b07a5939290628fa7565
Fad F <fmzfads@gmail.com>
2022-01-21 23:40:51 +0500

    BaseCommand has a _fromDecorator prop now
     This is to differentiate between class and decorator commands

--- src/bases/command/BaseCommand.ts
+++ src/bases/command/BaseCommand.ts
@@ -1,11 +1,13 @@
 import { ICommand, ICommandData, ICommandExec } from '../../typings'
 
 class BaseCommand implements ICommand {
+    readonly _fromDecorator!: boolean
     public data!: ICommandData
     public exec!: ICommandExec
 
-    constructor(data?: ICommandData) {
+    constructor(data?: ICommandData, fromDecorator?: boolean) {
         if (data) this.data = data
+        this._fromDecorator = fromDecorator ?? false
     }
 }
 
