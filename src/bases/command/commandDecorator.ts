c8645a180d7b553640834688093790c7453c28e3
Fad F <fmzfads@gmail.com>
2022-01-21 23:41:14 +0500

    new prop added so changed super() according to that

--- src/bases/command/commandDecorator.ts
+++ src/bases/command/commandDecorator.ts
@@ -2,11 +2,11 @@ import { Class } from 'type-fest'
 import { ICommandData } from '../../typings'
 import BaseCommand from './BaseCommand'
 
-const command = (data: ICommandData) => {
+const command: ((data: ICommandData) => (Target: Class<BaseCommand>) => any) = (data: ICommandData) => {
     let main = (Target: Class<BaseCommand>) => {
         return class extends Target {
             constructor(...args: any[]) {
-                super(data, ...args)
+                super(data, true, ...args)
             }
         }
     }
