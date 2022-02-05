19bb96ed0552e02a621bcff0c9a0597122b0b865
Fad F <fmzfads@gmail.com>
2022-02-05 17:37:45 +0500

    handlers are now imported together

---
+++ src/modules/handlers/index.ts
@@ -0,0 +1,9 @@
+import commandHandler from './command.handler'
+import eventHandler from './event.handler'
+import messageHandler from './message.handler'
+
+export {
+    commandHandler,
+    eventHandler,
+    messageHandler
+}
